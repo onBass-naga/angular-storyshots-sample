b00680c2b2989f7d33d85f753d4ad5c158d0046b
naga <naga@area-b.com>
2020-01-31 15:46:27 +0900

    multiple-projects-appのapplicationにhomeコンポーネントを追加し、notificationを読み込む実装追加

--- multiple-projects-app/projects/customer/src/app/app.module.ts
+++ multiple-projects-app/projects/customer/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {NotificationModule} from 'projects/common/src/lib/elements/notification/notification.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NotificationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
