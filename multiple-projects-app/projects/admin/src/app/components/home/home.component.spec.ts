41f9ee838de32a12aa3996dcd6a17364658c7366
naga <naga@area-b.com>
2020-02-02 20:51:05 +0900

    テストのワーニング除去

--- multiple-projects-app/projects/admin/src/app/components/home/home.component.spec.ts
+++ multiple-projects-app/projects/admin/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import {HomeComponent} from './home.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +9,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [HomeComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
